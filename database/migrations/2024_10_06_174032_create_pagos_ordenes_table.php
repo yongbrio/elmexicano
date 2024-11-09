12aa5eb771575225b8a5073c48dd82e494031137
ozock <jorgearmanbolivar@hotmail.com>
2024-11-09 18:03:43 -0500

    Ajuste de migracion

--- database/migrations/2024_10_06_174032_create_pagos_ordenes_table.php
+++ database/migrations/2024_10_06_174032_create_pagos_ordenes_table.php
@@ -11,24 +11,26 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pagos_ordenes', function (Blueprint $table) {
-            $table->id();
-            $table->string('orden');
-            $table->string('fecha');
-            $table->integer('id_empresa');
-            $table->string('nombre_empresa');
-            $table->integer('id_cuenta_banco');
-            $table->string('numero_cuenta_banco');
-            $table->integer('id_sucursal');
-            $table->string('nombre_sucursal');
-            $table->integer('id_forma_pago');
-            $table->string('nombre_forma_pago');
-            $table->integer('id_estado_pago');
-            $table->string('nombre_estado_pago');
-            $table->integer('dias_plazo_pago');
-            $table->string('monto');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('pagos_ordenes')) {
+            Schema::create('pagos_ordenes', function (Blueprint $table) {
+                $table->id();
+                $table->string('orden');
+                $table->string('fecha');
+                $table->integer('id_empresa');
+                $table->string('nombre_empresa');
+                $table->integer('id_cuenta_banco');
+                $table->string('numero_cuenta_banco');
+                $table->integer('id_sucursal');
+                $table->string('nombre_sucursal');
+                $table->integer('id_forma_pago');
+                $table->string('nombre_forma_pago');
+                $table->integer('id_estado_pago');
+                $table->string('nombre_estado_pago');
+                $table->integer('dias_plazo_pago');
+                $table->string('monto');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
